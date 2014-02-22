9d5570321b58ccf983fca7e72090308aa5ff40c7
Tim Reddehase <robustus@rightsrestricted.com>
2014-02-22 19:15:10 +0100

    document the iri generator.

--- lib/iri_generator.rb
+++ lib/iri_generator.rb
@@ -6,6 +6,16 @@ class IriGenerator
 
   class << self
 
+    # There are three ways to get an iri by calling this method.
+    # Each requires a repository as the first argument. We need
+    # the repository mainly for its path attribute.
+    #
+    # As a Second parameter one can specify either path: or ontology:
+    # ontology: Generate iri based on basepath of ontology
+    # path: Generate iri based on absolute file-system path
+    #
+    # The third option is to specify repository, ontology and child_iri
+    # which generates an iri for the child-ontology.
     def iri_for(repository, ontology: nil, path: nil, child_iri: nil)
       if ontology && child_iri
         determine_child_iri(repository, ontology, child_iri)
@@ -43,15 +53,20 @@ class IriGenerator
       ontohub_iri("#{repository.path}#{SEP}#{ontology.basepath}")
     end
 
+    # Safeguard against 'iris' from hets-output, which
+    # are parenthesized by angle-brackets.
     def clean(uncool_iri)
       uncool_iri.start_with?('<') ? uncool_iri[1..-2] : uncool_iri
     end
 
+    # reduces a path to the relevant sub-repository
+    # path for the file.
     def reduce(repository, path)
       repository_path = Regexp.escape(repository.path)
       match = path.match(%r{
         .+# match until repository_path is found
         (?<ontology_path>#{repository_path}.+)}x)
+      # return without file extension
       match['ontology_path'].split('.').first
     end
 
