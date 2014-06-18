04bbfa5209bb017e3e6dc6ab2f03ee37cd094c6b
Tim Reddehase <robustus@rightsrestricted.com>
2014-06-18 11:53:59 +0200

    replace iri-generator with more flexible wrapper
    That is the 'throw-it-all-out-and-do-it-again'-commit.

---
+++ lib/iri_generation.rb
@@ -0,0 +1,81 @@
+require 'ostruct'
+
+module IRIGeneration
+
+  # SEP is short for SEPARATOR
+  SEP = '/'
+  CHILD_SEP = '?'
+  PART_SEP = '#'
+
+  class Wrapper
+    attr_accessor :host, :port, :scheme, :subject
+
+    def initialize(host: nil, port: nil, scheme: nil)
+      self.host = host || Settings.hostname
+      self.port = port
+      self.scheme = scheme || Settings.scheme
+    end
+
+    def base
+      "#{scheme}://#{host}#{":#{port}" if port}"
+    end
+
+    def ontology=(ontology)
+      self.subject = ontology
+    end
+
+    def child_ontology=(ontology)
+      self.subject = ontology
+    end
+
+    def symbol=(symbol)
+      self.subject = symbol
+    end
+
+    def mapping=(mapping)
+      self.subject = mapping
+    end
+
+    def realize
+      h = OpenStruct.new(subject.expand_hierarchy)
+      [
+        base,
+        pathify(h.repository),
+        pathify(h.ontology),
+        childify(h.child_ontology),
+        childify(h.mapping),
+        partify(h.symbol),
+      ].join('')
+    end
+
+    def to_s
+      realize
+    end
+
+    private
+    def pathify(arg)
+      arg ? "#{SEP}#{arg}" : ''
+    end
+
+    def childify(arg)
+      arg ? "#{CHILD_SEP}#{arg}" : ''
+    end
+
+    def partify(arg)
+      arg ? "#{PART_SEP}#{arg}" : ''
+    end
+  end
+
+  def build_iri(*args, &block)
+    wrapper = Wrapper.new(*args)
+    wrapper.instance_eval(&block)
+    wrapper.realize
+  end
+
+  def iri_for(subject)
+    wrapper = Wrapper.new
+    wrapper.subject = subject
+    wrapper.to_s
+  end
+
+end
